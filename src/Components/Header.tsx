a9ecb09b55d4cecda60c03b8c7ed1257e21302cc
psc3245 <psc3.omaha@gmail.com>
2026-07-01 16:49:13 -0500

    work in progress

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,58 @@
-import "../Style/Header.css";
+import styles from "../Style/Header.module.css";
+import xIcon from "../Assets/x-button-icon.png";
+import hamburgerIcon from "../Assets/hamburger-menu-icon.png";
 
-function Header() {
+interface HeaderProps {
+  menuExpanded: boolean;
+  setMenuExpanded: (val: boolean) => void;
+}
+
+function Header({ menuExpanded, setMenuExpanded }: HeaderProps) {
   return (
-    <div className="menu-bar">
-      <div className="menu-button-section">
-        <button>Home</button>
+    <header className={styles.header}>
+      <div className={styles.leftSection}>
+        <div className={styles.menu}>
+          <div className={styles.menuButton}>
+            <button onClick={() => setMenuExpanded(!menuExpanded)}>
+              {menuExpanded ? (
+                <div><img src={xIcon} alt="x" /></div>
+              ) : (
+                <div><img src={hamburgerIcon} alt="hamburger" /></div>
+              )}
+            </button>
+          </div>
+
+          {menuExpanded && (
+            <div className={styles.dropdownMenu}>
+              <div className={styles.menuItem}>
+                <button>Home</button>
+              </div>
+              <div className={styles.menuItem}>
+                <button>Background</button>
+              </div>
+              <div className={styles.menuItem}>
+                <button>Projects</button>
+              </div>
+              <div className={styles.menuItem}>
+                <button>Contact</button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="menu-button-section">
-        <button>Background</button>
+
+      <div className={styles.logo}>{/* Logo goes here */}</div>
+
+      <div className={styles.rightSection}>
+        <input
+          type="text"
+          id="search"
+          className={styles.searchInput}
+          placeholder="Search..."
+        />
+        <div className={styles.searchIcon}>{/* Search icon */}</div>
       </div>
-      <div className="menu-button-section">
-        <button>Projects</button>
-      </div>
-      <div className="menu-button-section">
-        <button>Contact</button>
-      </div>
-    </div>
+    </header>
   );
 }
 
